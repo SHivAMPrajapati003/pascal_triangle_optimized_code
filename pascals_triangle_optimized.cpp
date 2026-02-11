41e950ef527b9c429f3485a6b828808c976ec227
SHivAMPrajapati003 <shivam2311063@akgec.ac.in>
2026-02-11 15:40:46 +0530

    pascals triangle optimised

---
+++ pascals_triangle_optimized.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the height of triagle.  ";
+    cin>>n;
+    for (int i = 0; i <= n-1; i++)
+    {
+      int curr = 1;
+      for (int j = 0; j <= i; j++)
+      {
+        cout<<curr<<" ";
+        curr = curr*(i-j)/(j+1);
+      }
+      cout<<endl;
+      
+    }
+}
